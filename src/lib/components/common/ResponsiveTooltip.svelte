aaccaf0e2b7cfd83014ce8ebc38ce905516630d0
shaunprice <shaundnz@gmail.com>
2023-10-25 18:58:45 +1300

    Added repsonsive tooltip

---
+++ src/lib/components/common/ResponsiveTooltip.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Breakpoints } from '$lib/constants';
+
+	export let breakpoint: Breakpoints;
+	export let tooltip: string;
+</script>
+
+<!-- Show tooltip -->
+<div class={`tooltip tooltip-right ${breakpoint}:hidden`} data-tip={tooltip}>
+	<slot />
+</div>
+<!-- Hide tooltip -->
+<div class={`hidden ${breakpoint}:block`}>
+	<slot />
+</div>
