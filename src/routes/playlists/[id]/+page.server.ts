c0916b61ef06939396bbe3e8df3e5003b2df2e0a
shaunprice <shaundnz@gmail.com>
2023-10-28 20:19:44 +1300

    Added mapping for date in playlist page load

--- src/routes/playlists/[id]/+page.server.ts
+++ src/routes/playlists/[id]/+page.server.ts
@@ -3,6 +3,11 @@ import { PlaylistsApi } from '$lib/api';
 export const load = async ({ params }) => {
 	const playlist = await PlaylistsApi.getPlaylist(params.id);
 
+	// Map the date string to an actual date object
+	playlist.playlistVersions.forEach((playlistVersion) => {
+		playlistVersion.versionDate = new Date(playlistVersion.versionDate);
+	});
+
 	return {
 		playlist: playlist
 	};
