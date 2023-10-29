7d692178c52b03c99b1821abcad60b3d126ab54c
shaunprice <shaundnz@gmail.com>
2023-10-29 13:51:24 +1300

    Updated sidebar to only show playlist image

--- src/lib/components/sidebar/PlaylistItem.svelte
+++ src/lib/components/sidebar/PlaylistItem.svelte
@@ -1,22 +1,17 @@
 <script lang="ts">
 	import type { PlaylistDto } from '$lib/contracts';
-	import ResponsiveTooltip from '../common/ResponsiveTooltip.svelte';
 
 	export let playlist: PlaylistDto;
 </script>
 
 <a href={`/playlists/${playlist.id}`}>
-	<ResponsiveTooltip tooltip={playlist.name}>
+	<div class="tooltip tooltip-right" data-tip={playlist.name}>
 		<div class="flex flex-row h-16 p-2 hover:bg-primary-focus rounded-lg">
 			<img
 				src={playlist.images[0].url}
 				alt={playlist.name}
 				class="w-12 aspect-square object-cover"
 			/>
-			<div class="hidden lg:block ml-3">
-				<div class="text-white">{playlist.name}</div>
-				<div class="text-sm">Playlist &bull; {playlist.owner.display_name}</div>
-			</div>
 		</div>
-	</ResponsiveTooltip>
+	</div>
 </a>
--- src/lib/components/sidebar/ResponsiveSidebar.svelte
+++ src/lib/components/sidebar/ResponsiveSidebar.svelte
@@ -1,29 +1,22 @@
 <script lang="ts">
 	import HomeIcon from '~icons/material-symbols/home-outline';
-	import GitBranchIcon from '~icons/mdi/source-branch';
 	import PlaylistItem from './PlaylistItem.svelte';
-	import ResponsiveTooltip from '../common/ResponsiveTooltip.svelte';
 	import type { PlaylistDto } from '$lib/contracts';
 
 	export let playlists: PlaylistDto[];
 </script>
 
-<div class="p-2 min-h-full w-20 lg:w-80 flex-shrink-0">
+<div class="p-2 min-h-full w-20 flex-shrink-0">
 	<a href="/">
-		<ResponsiveTooltip tooltip="Home">
+		<div class="tooltip tooltip-right" data-tip="Home">
 			<div
 				class="flex flex-row h-16 space-x-3 p-2 hover:bg-primary-focus rounded-lg items-center text-xl"
 			>
 				<HomeIcon class=" w-12 h-12 p-1" />
-				<h2 class="font-semibold hidden lg:block">Home</h2>
 			</div>
-		</ResponsiveTooltip>
+		</div>
 	</a>
 	<div class="divider my-0" />
-	<div class="flex flex-row h-16 p-2 space-x-3 rounded-lg items-center text-xl">
-		<GitBranchIcon class=" w-12 h-12 p-1" />
-		<h2 class="font-semibold hidden lg:block">Tracked Playlists</h2>
-	</div>
 	{#each playlists as playlist (playlist.id)}
 		<PlaylistItem {playlist} />
 	{/each}
