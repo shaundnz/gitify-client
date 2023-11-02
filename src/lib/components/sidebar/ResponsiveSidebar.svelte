cdf363711d5e467af43856de4908c5749b25b18a
shaunprice <shaundnz@gmail.com>
2023-11-03 09:50:57 +1300

    Simplified sidebar styles

--- src/lib/components/sidebar/ResponsiveSidebar.svelte
+++ src/lib/components/sidebar/ResponsiveSidebar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import GitIcon from '~icons/mdi/git';
 	import HomeIcon from '~icons/material-symbols/home-outline';
 	import PlaylistItem from './PlaylistItem.svelte';
 	import type { PlaylistDto } from '$lib/contracts';
@@ -7,12 +8,13 @@
 </script>
 
 <div class="p-2 min-h-full w-20 flex-shrink-0">
+	<div class="h-16">
+		<GitIcon class="w-16 h-16 text-[#1DB954]" />
+	</div>
 	<a href="/">
 		<div class="tooltip tooltip-right" data-tip="Home">
-			<div
-				class="flex flex-row h-16 space-x-3 p-2 hover:bg-primary-focus rounded-lg items-center text-xl"
-			>
-				<HomeIcon class=" w-12 h-12 p-1" />
+			<div class="flex h-16 p-2 hover:bg-primary-focus rounded-lg">
+				<HomeIcon class="w-12 h-12 p-1" />
 			</div>
 		</div>
 	</a>
