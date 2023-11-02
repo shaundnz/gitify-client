5a54ea31a15f63c34c255190e8d3bd814ea14ca9
shaunprice <shaundnz@gmail.com>
2023-11-03 09:48:38 +1300

    Added title to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,10 @@
 	export let data: PageData;
 </script>
 
-<main class="p-2">
+<main class="p-6">
+	<div>
+		<h1 class="text-white text-4xl font-bold mb-5">Tracked Playlists</h1>
+	</div>
 	<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 gap-6">
 		{#each data.playlists as playlist}
 			<PlaylistCard {playlist} />
