75fd07ddef60ece329690109a77df8df06849ee6
shaunprice <shaundnz@gmail.com>
2023-11-04 13:26:18 +1300

    Added trailing slash always to build settings to generate index.html files

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
 import { PlaylistsApi } from '$lib/api';
 
 export const prerender = true;
+export const trailingSlash = 'always';
 
 export const load = async () => {
 	const playlists = await PlaylistsApi.getAllPlaylists();
