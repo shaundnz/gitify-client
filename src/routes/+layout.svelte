c5f3b557729725a50926206a4ad198d44bc2f8b9
shaunprice <shaundnz@gmail.com>
2023-11-05 14:18:57 +1300

    Added footer to go to bottom of page when page content is less than 100vh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,38 +9,47 @@
 	export let data: PageData;
 </script>
 
-<div class="flex">
-	<ResponsiveSidebar playlists={data.playlists} />
-	<slot />
-</div>
-
-<footer class="footer flex justify-between items-center py-2 px-6 bg-primary text-primary-content">
-	<div class="pl-2">
-		<p>Created by Shaun Price</p>
+<div class="min-h-screen flex flex-col">
+	<div class="flex">
+		<ResponsiveSidebar playlists={data.playlists} />
+		<slot />
 	</div>
 
-	<nav>
-		<ul class="menu menu-horizontal p-0 [&>li>*]:px-2 [&>li>*]:rounded-md">
-			<li>
-				<a href="https://shaundnz.com" target="_blank" class="tooltip" data-tip="Personal Website">
-					<WebsiteIcon class="text-xl" />
-				</a>
-			</li>
-			<li>
-				<a href="https://github.com/shaundnz" target="_blank" class="tooltip" data-tip="Github">
-					<GithubIcon class="text-xl" />
-				</a>
-			</li>
-			<li>
-				<a
-					href="https://linkedin.com/in/shaun-price-71a1461b5"
-					target="_blank"
-					class="tooltip"
-					data-tip="LinkedIn"
-				>
-					<LinkedInIcon class="text-xl" />
-				</a>
-			</li>
-		</ul>
-	</nav>
-</footer>
+	<footer
+		class="footer flex justify-between items-center py-2 px-6 bg-primary text-primary-content mt-auto"
+	>
+		<div class="pl-2">
+			<p>Created by Shaun Price</p>
+		</div>
+
+		<nav>
+			<ul class="menu menu-horizontal p-0 [&>li>*]:px-2 [&>li>*]:rounded-md">
+				<li>
+					<a
+						href="https://shaundnz.com"
+						target="_blank"
+						class="tooltip"
+						data-tip="Personal Website"
+					>
+						<WebsiteIcon class="text-xl" />
+					</a>
+				</li>
+				<li>
+					<a href="https://github.com/shaundnz" target="_blank" class="tooltip" data-tip="Github">
+						<GithubIcon class="text-xl" />
+					</a>
+				</li>
+				<li>
+					<a
+						href="https://linkedin.com/in/shaun-price-71a1461b5"
+						target="_blank"
+						class="tooltip"
+						data-tip="LinkedIn"
+					>
+						<LinkedInIcon class="text-xl" />
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</footer>
+</div>
